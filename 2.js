a71b7a4732739b223abf9fbe90b9181146ef7740
Filip Nilsson <fillennn@hotmail.com>
2020-12-02 21:32:04 +0100

    Refined 2_1

--- 2.js
+++ 2.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 let strings = fs.readFileSync("data/2.txt", "utf-8").split("\n");
 let s
-let res = strings.filter(string => {
-	s = string.split(/-|\ |:\ /)
-	return new RegExp(`^([^${s[2]}]*${s[2]}[^${s[2]}]*){${s[0]},${s[1]}}$`).test(s[3])
-}).length
 
-console.log(res)
+const result1 = strings.reduce((count, string) => {
+	s = string.split(/-|\ |:\ /)
+	return count += new RegExp(`^([^${s[2]}]*${s[2]}[^${s[2]}]*){${s[0]},${s[1]}}$`).test(s[3])
+}, 0)
+
+console.log(result1)
